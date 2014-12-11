adacbc3b0a822f793cbe4f0e1988f835f461be38
noon <blahitsmev@gmail.com>
2014-12-11 13:02:23 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoonApp::Application.config.secret_token = 'b904607d29be8f3c77d68d1a1bd03ccf5b8311141e13cf1ead696ba3db7b41f674bfe9e4af530e754de2c304ef322921655e893e8dd31a35c3e73b0d81db321d'
